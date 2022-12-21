88db90b56615f419c445f6cee67ba0ae9c469246
santatra <razafindratsimba34@gmail.com>
2022-12-21 14:05:18 +0100

    Role Select in User views

--- Models/User.cs
+++ Models/User.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
 using System.Collections.Generic;
+using System.Web.Mvc;
 using System.Linq;
-using System.Web;
 using System.Security.Principal;
 
 namespace AppInventaire.Models
@@ -37,6 +37,22 @@ namespace AppInventaire.Models
             return new List<string> { 
                 "ID", "Prénom", "Nom", "E-Mail"
             };
+        }
+
+        public List<SelectListItem> GetRoleSelectListItems()
+        {
+            RoleRepository _rep = new RoleRepository();
+            List<SelectListItem> RoleSelectListItem = new List<SelectListItem>();
+            List<Role> roles = _rep.Fetch();
+            foreach (Role ro in roles)
+            {
+                RoleSelectListItem.Add(new SelectListItem()
+                {
+                    Text = $"{ro.RoleName}",
+                    Value = $"{ro.RoleName}"
+                });
+            };
+            return RoleSelectListItem;
         }
 
     }
