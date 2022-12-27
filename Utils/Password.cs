04a72bf6b98b0f114b5f433600024048762915a9
santatra <razafindratsimba34@gmail.com>
2022-12-27 10:08:44 +0100

    Add Utils/Password.cs

---
+++ Utils/Password.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppInventaire.Utils
+{
+    public class Password
+    {
+        public string passwd;
+        Password(string passwd)
+        {
+            this.passwd = passwd;
+        }
+        public bool CheckSpecialChar()
+        { 
+            List<string> special_chars = new List<string>()
+            { "&","ç","$","ù","%", "~","@",
+                "#","+","£", "é", "§", "?", "!",
+                "-", "è", "_", "à", "=", "^"
+            };
+            return special_chars.Any(s => passwd.Contains(s));
+        }
+    
+        public bool CheckLen(string passwd)
+        {
+            if(passwd.Length >= 8)
+            { return true; }
+            return false;
+        }
+    }
+
+    
+}
