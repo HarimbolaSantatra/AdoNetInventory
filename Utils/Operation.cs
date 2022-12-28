7d94e21cdeae8fd32dc7f98a5f9a3fcc8cc78d2a
santatra <razafindratsimba34@gmail.com>
2022-12-28 12:24:53 +0100

    Add Utils/Operation class & Add Sha1 hashing function in it

---
+++ Utils/Operation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+using System.Security.Cryptography;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppInventaire.Utils
+{
+    public class Operation
+    {
+        public static string Sha1Hash(string input)
+        {
+            // Hash User input password. Sha1 is the algorithm used in MySql table
+            SHA1 mySha1 = SHA1.Create();
+            // Convert LoginPassword string to Byte Array
+            byte[] inputBytes = Encoding.ASCII.GetBytes(input);
+            Byte[] hashed_inputBytes = mySha1.ComputeHash(inputBytes);
+            // Reconvert to string
+            string hashed_input = BitConverter.ToString(hashed_inputBytes).Replace("-", String.Empty).ToLower();
+
+            mySha1.Dispose();
+
+            return hashed_input;
+        }
+    }
+}
