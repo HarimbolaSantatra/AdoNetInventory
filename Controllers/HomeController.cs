1f0e2e21c1c64d478022639d022c4db3e63ff239
santatra <razafindratsimba34@gmail.com>
2022-12-30 10:03:55 +0100

    Delete contact & about views

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -32,20 +32,6 @@ namespace AppInventaire.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-
         [HttpPost]
         public ActionResult Login(FormCollection collection)
         {
