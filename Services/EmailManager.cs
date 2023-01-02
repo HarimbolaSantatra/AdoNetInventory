b85f4c69441af0c2ade112c7c3576afcc94b0d75
santatra <razafindratsimba34@gmail.com>
2023-01-02 11:18:55 +0100

    Add Email Services Class and Mailkit package.

--- Services/EmailManager.cs
+++ Services/EmailManager.cs
@@ -1,42 +1,50 @@
 ﻿using System;
 using System.Net;
 using System.Net.Mail;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+
+using MailKit.Net.Smtp;
+using MimeKit;
 
 namespace AppInventaire.Services
 {
     public class EmailManager
     {
-        MailAddress sender;
-        MailAddress receiver;
-        SmtpClient client;
-        public EmailManager(string senderAddress, string receiverAddress, string senderPassword)
+        MailAddress Sender;
+        MailAddress Receiver;
+        string Password;
+        string SmtpAddress;
+        int Port;
+        public EmailManager(string smtpAddress, int port)
         {
-            sender = new MailAddress(senderAddress);
-            receiver = new MailAddress(receiverAddress);
+            SmtpAddress = smtpAddress;
+            Port = port;
+        }
+
+        public void InitActor(string senderAddress, string receiverAddress, string senderPassword)
+        {
+            Sender = new MailAddress(senderAddress);
+            Receiver = new MailAddress(receiverAddress);
+            Password = senderPassword;
         }
         
         public void Send(string subject, string message)
         {
-            MailMessage Message = new MailMessage(sender, receiver);
+            MailMessage Message = new MailMessage(Sender, Receiver);
             Message.Subject = subject;
             Message.Body = message;
-            SmtpClient client = new SmtpClient("smtp.server.address", 2525)
+
+            using (var client = new MailKit.Net.Smtp.SmtpClient())
             {
-                Credentials = new NetworkCredential("smtp_username", password),
-                EnableSsl = true
-                // specify whether your host accepts SSL connections
-            };
-            // code in brackets above needed if authentication required
-            client.Send(message);
-            
-        }
+                // We use MailKit.Net.Smtp for SmtpClient becauce System.Net.Mail's SmtpClient class is not recommended: 
+                // voir lien: https://learn.microsoft.com/en-us/dotnet/api/system.net.mail.smtpclient?view=net-7.0
+                client.Connect(SmtpAddress, Port, false);
+                client.Authenticate(Sender.Address, Password);    // Code needed if authentication required
+                MimeMessage mimeMessage = (MimeMessage) Message;    // Convert System.Net.Mail.MailMessage to MimeKit.MimeMessage
+                client.Send(mimeMessage);
+                client.Disconnect(true);
+            }
 
-        public void Dispose()
-        {
-
+            Message.Dispose();
         }
 
     }
---
+++ Services/EmailSender.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppInventaire.Services
+{
+    public class EmailSender
+    {
+        public void NotifyCreateUser(string firstName, string lastName, string email, string roleName)
+        {
+            string Subject = "Ajout d'un nouveau utilisateur";
+            string Message = $@"
+Bonjour,
+
+Nous vous informons l'ajout d'un nouveau utilisateur ayant les informations suivants:
+    - Nom: {firstName}
+    - Prénom: {lastName}
+    - Rôle: {roleName}
+    - Adresse Mail: {email}
+
+Merci !
+";
+            EmailManager emailManager = new EmailManager("ssl0.ovh.net", 465);
+            emailManager.InitActor("andrana@crystal-frame.fr", "andrana@crystal-frame.fr", "$$SML99**md255");
+            emailManager.Send(Subject, Message);
+        }
+    }
+}
