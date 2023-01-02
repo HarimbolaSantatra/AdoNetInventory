25c7629a87b61c5c9834f57d1b66fd7192f00078
santatra <razafindratsimba34@gmail.com>
2023-01-02 09:11:49 +0100

    Add send email base code

---
+++ Services/EmailManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppInventaire.Services
+{
+    public class EmailManager
+    {
+        MailAddress sender;
+        MailAddress receiver;
+        SmtpClient client;
+        public EmailManager(string senderAddress, string receiverAddress, string senderPassword)
+        {
+            sender = new MailAddress(senderAddress);
+            receiver = new MailAddress(receiverAddress);
+        }
+        
+        public void Send(string subject, string message)
+        {
+            MailMessage Message = new MailMessage(sender, receiver);
+            Message.Subject = subject;
+            Message.Body = message;
+            SmtpClient client = new SmtpClient("smtp.server.address", 2525)
+            {
+                Credentials = new NetworkCredential("smtp_username", password),
+                EnableSsl = true
+                // specify whether your host accepts SSL connections
+            };
+            // code in brackets above needed if authentication required
+            client.Send(message);
+            
+        }
+
+        public void Dispose()
+        {
+
+        }
+
+    }
+}
