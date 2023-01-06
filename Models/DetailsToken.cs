d4209aca9e99c1129edf96c8e8c5794c73a5e262
santatra <razafindratsimba34@gmail.com>
2023-01-06 15:13:01 +0100

    Refac: Add Token superclass and inherit DetailsToken

--- Models/DetailsToken.cs
+++ Models/DetailsToken.cs
@@ -5,20 +5,9 @@ using System.Web;
 
 namespace AppInventaire.Models
 {
-    public class DetailsToken
+    public class DetailsToken : Token
     {
         public int UserId { get; set; }         // Owner of the token
         public int AddedUserId { get; set; }    // ID of the new user
-        public DateTime CreationDate { get; set; }
-        public string TokenKey { get; set; }
-        public DateTime ExpirationDate
-        {
-            get { return CreationDate + new TimeSpan(24, 0, 0); } // Date d'expiration: dans 24 heures
-        }
-
-        public bool isExpired()
-        {
-            return DateTime.Now >= ExpirationDate;
-        }
     }
 }
---
+++ Models/Token.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppInventaire.Models
+{
+    public class Token
+    {
+        public string TokenKey { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime ExpirationDate
+        {
+            get { return CreationDate + new TimeSpan(24, 0, 0); } // Date d'expiration: dans 24 heures
+        }
+
+        public bool isExpired()
+        {
+            return DateTime.Now >= ExpirationDate;
+        }
+    }
+}
